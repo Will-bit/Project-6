2aad39b83381d46365575212c6049d97dc71e314
William Woods <54908283+Will-bit@users.noreply.github.com>
2019-09-26 10:39:31 -0500

    Add files via upload

---
+++ Secret Project/index.js
@@ -0,0 +1,45 @@
+
+//var background = new Image();
+//background.src = "assets/images/space.jpg";
+
+//var imgShip = document.getElementById("ship");
+//let canvas = document.getElementById("gameScreen");
+
+//let ctx = canvas.getContext("2d");
+
+
+//const GAME_WIDTH = 800;
+//const GAME_HEIGHT = 600;
+
+//background.onload = function(){
+  //  ctx.drawImage(background, canvas.width, canvas.height);
+//}
+///let paddle = new Paddle(GAME_WIDTH, Game_Height);
+
+//paddle.draw(ctx);
+
+
+/// Clears entire screen; Could be a problem when introducing enemy ships.  Likely
+ 
+ 
+//ctx.drawImage(imgShip, 10, 10, 100, 100);
+/*
+let lastTime = 0;
+
+function gameLoop(timestamp){
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+
+    ctx.clearRect(0, 0, 800, 600);
+    ctx.update();
+    ctx.drawImage(imgShip, 10, 10, 100, 100);
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+
+// ctx.fillRect(20, 20, 100, 100);
+
+*/
